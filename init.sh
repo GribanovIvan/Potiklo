4500052351af1c82b5d13ceaed7226c102d430d7
GribanovIvan <van.a.gribanov@gmail.com>
2024-07-27 03:49:03 +0300

    update

--- init.sh
+++ init.sh
@@ -8,6 +8,7 @@ curl -O $(curl 'https://www.mediafire.com/download/ub48kk5i1rmsrgw/WildCaves3-0.
 curl -LO https://github.com/KryptonCaptain/ThaumicUtilities/releases/download/0.3.12/Thaumic-Utilities-.1.7.10.0.3.12-0.jar
 curl -LO https://github.com/KryptonCaptain/Misc-Files/raw/master/other%20mods/MagiaNaturalis-1.7.10-0.1.5b.jar
 curl -LO https://github.com/makamys/CoreTweaks/releases/download/0.3.3.2/CoreTweaks-1.7.10-0.3.3.2+nomixin.jar
+curl -LO https://github.com/GTNewHorizons/Applied-Energistics-2-Unofficial/releases/download/rv3-beta-415-GTNH/appliedenergistics2-rv3-beta-415-GTNH.jar
 mkdir -p ../scripts
 cd ../scripts
 curl -O https://mediafilez.forgecdn.net/files/4192/999/1.7.10-AetherLegacy-Aspects.zs
@@ -17,7 +18,7 @@ curl https://mediafilez.forgecdn.net/files/2560/927/ChiselAspect.zs |grep -v blo
 curl -O https://mediafilez.forgecdn.net/files/2535/602/Legacy%20Scripts.zip
 unzip -o Legacy*Scripts.zip '8x Stairs - Applied Energistics 2 (1.7).zs' '8x Stairs - Biomes O'\'' Plenty (1.7).zs' '8x Stairs - Chisel (1.7).zs' '8x Stairs - Thaumcraft (1.7).zs' '8x Stairs - Vanilla (1.7).zs'
 rm Legacy*Scripts.zip
-curl https://mediafilez.forgecdn.net/files/3598/340/ExtraTiC-patches-Thaumcraft-1.2.zs | grep -v mana >ExtraTiC-patches-Thaumcraft-1.2_withoutmana.zs
+#curl https://mediafilez.forgecdn.net/files/3598/340/ExtraTiC-patches-Thaumcraft-1.2.zs | grep -v mana >ExtraTiC-patches-Thaumcraft-1.2_withoutmana.zs
 curl -O https://mediafilez.forgecdn.net/files/2313/159/IronChests%20%2B%20Extra%20Carts.zs
 cd ..
 curl -O https://legionfonts.com/storage/archives/Minecraft%201.1.zip
